fa2d2c6781f0211aed45e8a8b5ef15534b0e636f
hkshen <308421189@qq.com>
2015-06-05 09:12:40 +0800

    Initial Commit

---
+++ codecOfiLBC/codecOfiLBC.h
@@ -0,0 +1,13 @@
+//
+//  codecOfiLBC.h
+//  codecOfiLBC
+//
+//  Created by shenkun on 15/6/5.
+//  Copyright (c) 2015年 shenkun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface codecOfiLBC : NSObject
+
+@end
